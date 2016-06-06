628be499ea6bcaa64058c45245bb941f72422c68
tobywhughes <tobywhughes@gmail.com>
2016-06-05 23:30:06 -0700

    Finished the parsing part of the parsing module. Now just to add some bells and whistles and cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,17 @@ mod tests{
 
 	#[test]
 	fn it_works(){
-		//Basic URL
-		let y = "http://www.google.com".to_string();
-		let mut x : String = encode::encode_url_string(y);
-		println!("------------------------------");
-		assert_eq!("http%3A%2F%2Fwww.google.com", x);
-		x = decode::decode_url_string(x);
-		assert_eq!("http://www.google.com", x);
+		//URL Encode/Decode Tests
+		let mut enc_url : String= "http://www.google.com".to_string();
+		enc_url = encode::encode_url_string(enc_url);
+		assert_eq!("http%3A%2F%2Fwww.google.com", enc_url);
+		enc_url = decode::decode_url_string(enc_url);
+		assert_eq!("http://www.google.com", enc_url);
+		enc_url = String::from("http://www.google.com/path;type=a?query#fragment");
+		enc_url = encode::encode_url_string(enc_url);
+		assert_eq!("http%3A%2F%2Fwww.google.com%2Fpath%3Btype%3Da%3Fquery%23fragment", enc_url);
+		enc_url = decode::decode_url_string(enc_url);
+		assert_eq!("http://www.google.com/path;type=a?query#fragment", enc_url);
 
 		//Spaces
 		let space_test = "asdf asdf".to_string();
@@ -29,14 +33,22 @@ mod tests{
 		//Url Parser
 		let w_scheme: String = String::from("http://www.google.com");
 		let wo_scheme: String = String::from("www.google.com");
-		let wparams: String = String::from("www.google.com/seeme");
+		let wparams: String = String::from("http://www.google.com/path/path");
+		let everything : String = String::from("http://www.google.com/path;type=a?query#fragment");
 
+		//Parsed Urls
+		let purl1 = parse::url_parse(w_scheme);
+		let purl2 = parse::url_parse(wo_scheme);
+		let purl3 = parse::url_parse(wparams);
+		let purl4 = parse::url_parse(everything);
 
-		parse::url_parse(w_scheme);
-		parse::url_parse(wo_scheme);
-		parse::url_parse(wparams);
-
-		//Temp so test prints
-		assert_eq!(1,2);
+		//Parsed Tests
+		assert_eq!(purl1.scheme, "http");
+		assert_eq!(purl2.scheme, "");
+		assert_eq!(purl3.net_loc, "www.google.com");
+		assert_eq!(purl3.path, "/path/path");
+		assert_eq!(purl4.params, ";type=a");
+		assert_eq!(purl4.query, "?query");
+		assert_eq!(purl4.frag, "#fragment");	
 	}
 }
--- src/parsing/parse.rs
+++ src/parsing/parse.rs
@@ -8,15 +8,20 @@ pub fn url_parse(url : String) -> ParsedUrl{
 
 	purl.net_loc = get_net_loc(&url, purl.scheme.len());
 
-	//So that the compiler doesn't complain while implementing
-	purl.path = purl.path;
-	purl.params = purl.params;
-	purl.query = purl.query;
-	purl.frag = purl.frag;
+	//Extra length to scheme if it exits (purl.scheme doesn't account for :// after scheme)
+	let mut scheme_extra : usize = 0;
+	if purl.scheme.len() != 0{
+			scheme_extra = 3;
+	}
 
-	//Quick tests
-	println!("{}", purl.scheme);
-	println!("{}", purl.net_loc);
+	//Cuts off the scheme and net_loc
+	let cut_url : String = cut_start(&url, purl.scheme.len() + purl.net_loc.len() + scheme_extra);
+
+	purl.path = get_path(&cut_url);
+	purl.params = get_params(&cut_url);
+	purl.query = get_query(&cut_url);
+	purl.frag = get_frag(&cut_url);
+
 	purl
 }
 
@@ -79,11 +84,138 @@ fn get_net_loc(url: &String, mut scheme_len: usize) -> String{
 	net_string
 }
 
+fn get_path(url : &String) -> String {
+	let mut path_flag : bool= false;
+	let mut path_string : String = "".to_string();
+
+	lazy_static!{
+		static ref RE : Regex = Regex::new(r"[;\?#]").unwrap();
+	}
+
+	for c in url.chars(){
+		if path_flag == false {
+			if c == '/'{
+				path_flag = true;
+				path_string.push(c);
+			}
+		}
+		else {
+			if RE.is_match(&c.to_string()) {
+				break;
+			}
+			else{
+				path_string.push(c);
+			}
+		}
+	}
+
+	path_string
+}
+
+fn get_params(url : &String) -> String {
+	let mut param_flag : bool= false;
+	let mut param_string : String = "".to_string();
+
+	lazy_static!{
+		static ref RE : Regex = Regex::new(r"[/\?#]").unwrap();
+	}
+
+	for c in url.chars(){
+		if param_flag == false {
+			if c == ';'{
+				param_flag = true;
+				param_string.push(c);
+			}
+		}
+		else {
+			if RE.is_match(&c.to_string()) {
+				break;
+			}
+			else{
+				param_string.push(c);
+			}
+		}
+	}
+
+	param_string
+}
+
+fn get_query(url : &String) -> String {
+	let mut query_flag : bool= false;
+	let mut query_string : String = "".to_string();
+
+	lazy_static!{
+		static ref RE : Regex = Regex::new(r"[/;#]").unwrap();
+	}
+
+	for c in url.chars(){
+		if query_flag == false {
+			if c == '?'{
+				query_flag = true;
+				query_string.push(c);
+			}
+		}
+		else {
+			if RE.is_match(&c.to_string()) {
+				break;
+			}
+			else{
+				query_string.push(c);
+			}
+		}
+	}
+
+	query_string
+}
+
+fn get_frag(url : &String) -> String {
+	let mut frag_flag : bool= false;
+	let mut frag_string : String = "".to_string();
+
+	lazy_static!{
+		static ref RE : Regex = Regex::new(r"[/;\?]").unwrap();
+	}
+
+	for c in url.chars(){
+		if frag_flag == false {
+			if c == '#'{
+				frag_flag = true;
+				frag_string.push(c);
+			}
+		}
+		else {
+			if RE.is_match(&c.to_string()) {
+				break;
+			}
+			else{
+				frag_string.push(c);
+			}
+		}
+	}
+
+	frag_string
+}
+
+fn cut_start(url : &String, mut del_len : usize) -> String{
+	let mut cut_string : String = "".to_string();
+
+	for c in url.chars(){
+		if del_len != 0 {
+			del_len -= 1;
+		}
+		else {
+			cut_string.push(c);
+		}
+	}
+
+	cut_string
+}
+
 pub struct ParsedUrl {
-	scheme: String,
-	net_loc: String,
-	path: String,
-	params: String,
-	query: String,
-	frag: String,
+	pub scheme: String,
+	pub net_loc: String,
+	pub path: String,
+	pub params: String,
+	pub query: String,
+	pub frag: String,
 }
